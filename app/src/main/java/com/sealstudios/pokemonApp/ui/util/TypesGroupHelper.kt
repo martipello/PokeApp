843e9c51ede940612499b0b8ce446b4a1112fcd2
martin seal <martin.seal@spicatech.co.uk>
2020-09-26 21:40:42 +0100

    split filter and type group helper

---
+++ app/src/main/java/com/sealstudios/pokemonApp/ui/util/TypesGroupHelper.kt
@@ -0,0 +1,30 @@
+package com.sealstudios.pokemonApp.ui.util
+
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+
+class TypesGroupHelper(
+    private val chipGroup: ChipGroup,
+    private val pokemonTypes: List<PokemonType>
+) {
+    fun bindChips() {
+        for (x in pokemonTypes.indices) {
+            val pokemonType = pokemonTypes[x]
+            chipGroup.getChildAt(x).apply {
+                this as Chip
+                text = pokemonType.name.capitalize()
+                chipIcon = ContextCompat.getDrawable(
+                    context,
+                    pokemonType.icon
+                )
+                setChipBackgroundColorResource(pokemonType.color)
+                isCheckable = false
+                isClickable = false
+                rippleColor = null
+                visibility = View.VISIBLE
+            }
+        }
+    }
+}

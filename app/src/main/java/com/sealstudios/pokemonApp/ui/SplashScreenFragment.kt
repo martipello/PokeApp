9f079d4462bd37007c8b8ea9dd0853efc165f128
martin seal <martin.seal@spicatech.co.uk>
2020-10-21 00:12:48 +0100

    changed splash screen to use ktx animation extensions

--- app/src/main/java/com/sealstudios/pokemonApp/ui/SplashScreenFragment.kt
+++ app/src/main/java/com/sealstudios/pokemonApp/ui/SplashScreenFragment.kt
@@ -6,12 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewAnimationUtils
 import android.view.ViewGroup
-import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.get
 import com.sealstudios.pokemonApp.R
 import com.sealstudios.pokemonApp.databinding.SplashScreenContainerBinding
+import com.sealstudios.pokemonApp.ui.listenerExtensions.awaitTransitionEnd
+import com.sealstudios.pokemonApp.ui.listenerExtensions.startAndWait
+import kotlinx.coroutines.launch
 import kotlin.math.hypot
 
 
@@ -28,36 +31,33 @@ class SplashScreenFragment : Fragment() {
         return binding.root
     }
 
-    //TODO allowEnterTransitionOverlap
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setMotionLayoutListener()
+        handleEnterAnimation()
         binding.splashScreen.motionRoot.transitionToState(R.id.pokeball_landed)
     }
 
-    private fun setMotionLayoutListener() {
-        binding.splashScreen.motionRoot.setTransitionListener(object :
-            MotionLayout.TransitionListener {
-            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+    private fun handleEnterAnimation() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            binding.splashScreen.motionRoot.run {
+                awaitTransitionEnd()
                 createRevealAnimation()
             }
-
-            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {}
-            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {}
-            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
-        })
+        }
     }
 
     private fun createRevealAnimation() {
         val x: Int = binding.root.right / 2
         val y: Int = binding.root.bottom - binding.root.bottom / 9
         val endRadius = hypot(binding.root.width.toDouble(), binding.root.height.toDouble()).toInt()
-        val anim = createCircleRevealAnimator(x, y, endRadius)
-        addCircleRevealAnimationListener(anim)
-        anim.duration = 250
-        binding.pokeballOpen.visibility = View.VISIBLE
-        anim.start()
+        viewLifecycleOwner.lifecycleScope.launch {
+            createCircleRevealAnimator(x, y, endRadius).run {
+                duration = 250
+                binding.pokeballOpen.visibility = View.VISIBLE
+                startAndWait()
+                navigateToListFragment()
+            }
+        }
     }
 
     private fun createCircleRevealAnimator(x: Int, y: Int, endRadius: Int): Animator {
@@ -66,20 +66,6 @@ class SplashScreenFragment : Fragment() {
             0f,
             endRadius.toFloat()
         )
-    }
-
-    private fun addCircleRevealAnimationListener(anim: Animator) {
-        anim.addListener(object : Animator.AnimatorListener {
-            override fun onAnimationStart(animator: Animator) {
-                navigateToListFragment()
-            }
-
-            override fun onAnimationEnd(animator: Animator) {
-            }
-
-            override fun onAnimationCancel(animator: Animator) {}
-            override fun onAnimationRepeat(animator: Animator) {}
-        })
     }
 
     private fun navigateToListFragment() {

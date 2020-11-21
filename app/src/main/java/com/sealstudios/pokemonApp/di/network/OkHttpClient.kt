1957fa95bac31b08148549a0020e3f12792b5fec
martin seal <martin.seal@spicatech.co.uk>
2020-11-21 20:37:24 +0000

    REFACTORED http client

--- app/src/main/java/com/sealstudios/pokemonApp/di/network/OkHttpClient.kt
+++ app/src/main/java/com/sealstudios/pokemonApp/di/network/OkHttpClient.kt
@@ -1,33 +1,27 @@
 package com.sealstudios.pokemonApp.di.network
 
 import com.sealstudios.pokemonApp.BuildConfig
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit
-import javax.inject.Singleton
 
-@Module
-@InstallIn(ApplicationComponent::class)
 class OkHttpClient {
-    @Provides
-    @Singleton
-    fun provideOkHttpClient(): okhttp3.OkHttpClient.Builder {
-        val okHttpClient = okhttp3.OkHttpClient.Builder()
-        okHttpClient.connectTimeout(2, TimeUnit.MINUTES)
-        okHttpClient.readTimeout(2, TimeUnit.MINUTES)
-        okHttpClient.writeTimeout(2, TimeUnit.MINUTES)
 
-        when {
-            BuildConfig.DEBUG -> {
-                val logging = HttpLoggingInterceptor()
-                logging.level = HttpLoggingInterceptor.Level.BODY
-                okHttpClient.addInterceptor(logging)
+    companion object {
+
+        fun getOkHttpClient(): okhttp3.OkHttpClient.Builder {
+            val okHttpClient = okhttp3.OkHttpClient.Builder()
+            okHttpClient.connectTimeout(2, TimeUnit.MINUTES)
+            okHttpClient.readTimeout(2, TimeUnit.MINUTES)
+            okHttpClient.writeTimeout(2, TimeUnit.MINUTES)
+
+            when {
+                BuildConfig.DEBUG -> {
+                    val logging = HttpLoggingInterceptor()
+                    logging.level = HttpLoggingInterceptor.Level.BODY
+                    okHttpClient.addInterceptor(logging)
+                }
             }
+            return okHttpClient
         }
-
-        return okHttpClient
     }
 }
--- app/src/main/java/com/sealstudios/pokemonApp/di/network/PokemonServiceProvider.kt
+++ app/src/main/java/com/sealstudios/pokemonApp/di/network/PokemonServiceProvider.kt
@@ -2,44 +2,26 @@ package com.sealstudios.pokemonApp.di.network
 
 import com.sealstudios.pokemonApp.BuildConfig
 import com.sealstudios.pokemonApp.api.service.PokemonService
+import com.sealstudios.pokemonApp.di.network.OkHttpClient.Companion.getOkHttpClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
 class PokemonServiceProvider {
-
     @Provides
     @Singleton
     fun providePokemonService(): PokemonService {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .client(provideOkHttpClient().build())
+            .client(getOkHttpClient().build())
             .build()
             .create(PokemonService::class.java)
-    }
-
-    private fun provideOkHttpClient(): okhttp3.OkHttpClient.Builder {
-        val okHttpClient = okhttp3.OkHttpClient.Builder()
-        okHttpClient.connectTimeout(2, TimeUnit.MINUTES)
-        okHttpClient.readTimeout(2, TimeUnit.MINUTES)
-        okHttpClient.writeTimeout(2, TimeUnit.MINUTES)
-
-        when {
-            BuildConfig.DEBUG -> {
-                val logging = HttpLoggingInterceptor()
-                logging.level = HttpLoggingInterceptor.Level.BODY
-                okHttpClient.addInterceptor(logging)
-            }
-        }
-        return okHttpClient
     }
 }
